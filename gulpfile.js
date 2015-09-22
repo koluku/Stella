731a688e1282d233558ce60c138dfb90a1fca2fe
koluku <korufiru@gmail.com>
2015-09-22 14:02:24 +0900

    HTML記述からJade記述に変更
    作業効率を上げるためHTMLテンプレートを使用

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@ var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var compass = require('gulp-compass');
+var jade = require('gulp-jade');
 var notify  = require('gulp-notify');
 var plumber = require('gulp-plumber');
 
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './html/'
+    }
+  });
+});
 gulp.task('compass', function() {
   gulp.src('./html/scss/*.scss')
     .pipe(plumber({
@@ -18,16 +26,22 @@ gulp.task('compass', function() {
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('./html'));
+    .pipe(gulp.dest('./html'))
+    .pipe(browserSync.stream());
 });
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './html/'
-    }
-  });
+gulp.task('jade', function() {
+  gulp.src('./html/jade/*.jade')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./html'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['compass','browser-sync'], function() {
-  gulp.watch(['./html/scss/*.scss'], ['compass']).on('change', browserSync.reload);;
+  gulp.watch(['./html/scss/*.scss'], ['compass']);
+  gulp.watch(['./html/jade/*.jade'], ['jade']);
 });
